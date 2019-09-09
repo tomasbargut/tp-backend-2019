cf602015e40dd45544b3d0a26e3aa57595da7f2b
Tomás Bargut <tomasbarguttabbaj@gmail.com>
2019-09-08 23:06:57 -0300

    Mejora en api mensajes

--- src/api/message.js
+++ src/api/message.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { MessageService } = require('../services');
 
 const message_router = express.Router();
+const messageService = new MessageService();
 
 message_router.get('/', async (req, res) => {
     res.send('Unos mensajes');
@@ -8,13 +10,24 @@ message_router.get('/', async (req, res) => {
 
 // TODO: Implementar
 message_router.get('/:messageid', async (req, res) => {
-    res.send('El mensage: ' + req.params.messageid);
+    const message = await messageService.findById(req.params.messageid, req.user);
+    if(!message) {
+        return res.status(401).send('Unauthorized');
+    }
+    return res.status(200).json(message);
 })
 
 // TODO: Implementar
 message_router.post('/', async (req, res) => {
     const messageData = req.body;
-    res.send('Creado');
+    const {err, message} = await messageService.create({
+        ...messageData,
+        emiter: req.user.id
+    });
+    if(err) {
+        return res.status(400).json(err);
+    }
+    return res.status(201).json(message);
 });
 
 module.exports = message_router;
--- src/services/MessageService.js
+++ src/services/MessageService.js
@@ -1,5 +1,20 @@
 const {Message} = require('../models');
+const UserService = require('./UserService');
 
+const userService = new UserService();
 module.exports = class MessageService {
-    
+    async findById(id, user) {
+        const message = await Message.findById(id);
+        return (message.emiter == user.id || message.receptor == user.id) ? message: null;  
+    }
+
+    async create(messageData) {
+        const receptor = await userService.getOneById(messageData.receptor);
+        if(!receptor) {
+            return {err:"No existe el receptor", message: null};
+        }
+        messageData = {...messageData, date: Date()};
+        const message = await Message.create(messageData);
+        return {err: null, message};
+    }
 }
